26dbb36c6db87c67c9dc861d8e98ab14d72662c6
Angel200270 <avillalpando@uadec.edu.mx>
2025-10-28 23:52:55 -0600

    el diagrama de clases y el catalogo de solicitudes

---
+++ TicketDeTurno/TicketDeTurno/Views/Catalogos/Solicitudes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicketDeTurno.Views.Catalogos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
